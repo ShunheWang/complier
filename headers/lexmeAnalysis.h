c2934d9852d00cc1718188f9b652ae7f0eb39dd1
Shunhe <maisonwangsh@gmail.com>
2021-01-08 21:50:35 +1100

    Revert "record"
    This reverts commit cde3e4786672b1d9608aedb828d1c043593db4cb.

--- headers/lexmeAnalysis.h
+++
@@ -1,9 +0,0 @@
-#ifndef LEXMEANALYSIS_H
-#define LEXMEANALYSIS_H
-
-void doLexmeAnalysis();
-void error();
-
-
-
-#endif // LEXMEANALYSIS_H
--- headers/preprocessing.h
+++
@@ -1,5 +0,0 @@
-void closeFile(const FILE *fp);
-int deleteComments(int intc,char *outc);
-int deleteEnters(const int c, int *counterp);
-int deleteSpaces(const int c);
-void doPreprocessing();
